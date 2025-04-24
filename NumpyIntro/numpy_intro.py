d37694e9b9af9ac36b083b80d8f91141e0b096d2
Ana <baconan@oregonstate.edu>
2025-04-24 18:57:13 +0000

    worked through problem 3 on lab 3

--- NumpyIntro/numpy_intro.py
+++ NumpyIntro/numpy_intro.py
@@ -1,8 +1,8 @@
 # numpy_intro.py
 """Python Essentials: Intro to NumPy.
-<Name>
-<Class>
-<Date>
+<Ana Bacon>
+<MTH 420>
+<04/18/2025>
 """
 
 import numpy as np
@@ -10,12 +10,27 @@ import numpy as np
 
 def prob1():
     """ Define the matrices A and B as arrays. Return the matrix product AB. """
-    raise NotImplementedError("Problem 1 Incomplete")
-
-
+    
+    A = np.array([[3, -1, 4],
+                  [1, 5, -9]])
+    B = np.array([[2, 6, -5,3],
+                  [5, -8, 9, 7],
+                  [9, -3, -2, -3]])
+    C = (A@B)
+    
+    print(C)
+    
 def prob2():
     """ Define the matrix A as an array. Return the matrix -A^3 + 9A^2 - 15A. """
-    raise NotImplementedError("Problem 2 Incomplete")
+    A = np.array([[3, 1, 4],
+                 [1, 5, 9],
+                 [-5, 3, 1]])
+    A2 = A @ A
+    A3 = A @ A2
+    return -(A3) + (9*A2) - (15*A)
+result = prob2()
+print(result)
+    
 
 
 def prob3():
@@ -23,7 +38,23 @@ def prob3():
     this section of the manual (not np.array()). Calculate the matrix product ABA,
     change its data type to np.int64, and return it.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    A = np.zeros((7, 7), dtype=np.int64) 
+    A[np.triu_indices(7)]=1
+    
+    B = np.ones((7, 7), dtype=np.int64)
+    B[np.triu_indices(7)]=5
+    B[np.tril_indices(7, -1)]=-1
+    
+    print(A)
+    print(B)
+    
+    C = A @ B
+    
+    
+    return C @ A
+result = prob3()
+print(result)
+
 
 
 def prob4(A):
@@ -63,10 +94,23 @@ def prob6(A):
     """
     raise NotImplementedError("Problem 6 Incomplete")
 
-
+#dont do this one
 def prob7():
     """ Given the array stored in grid.npy, return the greatest product of four
     adjacent numbers in the same direction (up, down, left, right, or
     diagonally) in the grid. Use slicing, as specified in the manual.
     """
     raise NotImplementedError("Problem 7 Incomplete")
+    
+if __name__ == "__main__":
+    prob1()
+    
+    prob2()
+    
+    prob3()
+    
+    
+    
+    
+    
+    
