683397db9cadb5a001b3c79a926255b0cbb3676b
Ana <baconan@oregonstate.edu>
2025-04-25 21:22:58 +0000

    finished up most of lab 3

--- NumpyIntro/numpy_intro.py
+++ NumpyIntro/numpy_intro.py
@@ -57,7 +57,7 @@ print(result)
 
 
 
-def prob4(A):
+def prob4():
     """ Make a copy of 'A' and use fancy indexing to set all negative entries of
     the copy to 0. Return the resulting array.
 
@@ -66,7 +66,18 @@ def prob4(A):
         >>> prob4(A)
         array([0, 0, 3])
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    A = np.array([[-17, 16, -15, 14, -13, 12],
+                [-11, 10, -9, 8, -7, 6],
+                 [-5, 4, -3, 2, -1, 0]])
+                    
+    mask = A < 0 
+    mask
+    A[mask] = 0
+    
+    
+    print(A)
+    
+
 
 
 def prob5():
@@ -78,7 +89,38 @@ def prob5():
     where I is the 3x3 identity matrix and each 0 is a matrix of all zeros
     of the appropriate size.
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+    
+    A = np.array([[0, 2, 4],
+                  [1, 3, 5]])
+    B = np.ones((3, 3), dtype=np.int64)
+    B[np.triu_indices(3)]= 0
+    B[np.tril_indices(3, 0)]= 3
+    
+    C = np.diag([-2, -2, -2])
+    
+    I = np.eye(3)
+    
+    
+    print(A)
+    print(B)
+    print(C)
+    
+    A0 = np.zeros((2, 3))  
+    B0 = np.zeros((3, 2))  
+    C0 = np.zeros((3, 3)) 
+    A1 = A.T
+
+    row1 = np.hstack((A0, A1, I)) 
+    
+    row2 = np.hstack((A, A0, B0))
+
+    row3 = np.hstack((B, C0, C))
+    
+    block_matrix = np.vstack((row1, row2, row3))
+    
+    print(block_matrix)
+    
+    
 
 
 def prob6(A):
@@ -92,7 +134,22 @@ def prob6(A):
                [ 0.        ,  1.        ,  0.        ],
                [ 0.33333333,  0.33333333,  0.33333333]])
     """
-    raise NotImplementedError("Problem 6 Incomplete")
+def row_stochastic(matrix):
+    
+    row_sums = matrix.sum(axis=1, keepdims=True)  
+
+    stochastic_matrix = matrix / row_sums
+    
+    return stochastic_matrix 
+
+    A = np.array([[1, 1, 0],
+                  [0, 1, 0],
+                  [1, 1, 1]])
+    print(A)
+
+    result = row_stochastic(A)
+    print(result)    
+    
 
 #dont do this one
 def prob7():
@@ -109,8 +166,9 @@ if __name__ == "__main__":
     
     prob3()
     
-    
-    
-    
-    
+    prob4()
+                 
+    prob5()
+                 
+    prob6()
     
